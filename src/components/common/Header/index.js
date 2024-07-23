67108dd6dc58e073250109835d36abcbd894524e
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-07-23 09:26:10 +0200

    Update

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./styles.css";
 import { Link, useLocation } from "react-router-dom";
+import FavouriteRoundedIcon from '@material-ui/icons/FavoriteRounded'; // Example import, adjust as needed
+import Elements from './Elements'; // Adjust this import to your actual file
 
-function Header() {
+function Header({ currentUser }) {
   const location = useLocation();
   const currentPath = location.pathname;
 
@@ -10,27 +12,26 @@ function Header() {
     <div className="navbar">
       <div className="gradient"></div>
       <div className="links">
-        <Link to="/" className={currentPath == "/" ? "active" : ""}>
+        <Link to="/" className={currentPath === "/" ? "active" : ""}>
           Signup
         </Link>
-        <Link
-          to="/podcasts"
-          className={currentPath == "/podcasts" ? "active" : ""}
-        >
+        <Link to="/podcasts" className={currentPath === "/podcasts" ? "active" : ""}>
           Podcasts
         </Link>
-        <Link
-          to="/create-a-podcast"
-          className={currentPath == "/create-a-podcast" ? "active" : ""}
-        >
+        <Link to="/create-a-podcast" className={currentPath === "/create-a-podcast" ? "active" : ""}>
           Start A Podcast
         </Link>
-        <Link
-          to="/profile"
-          className={currentPath == "/profile" ? "active" : ""}
-        >
+        <Link to="/profile" className={currentPath === "/profile" ? "active" : ""}>
           Profile
         </Link>
+        {currentUser && (
+          <Link to='/favourites' style={{ textDecoration: "none", color: "inherit", width: '100%' }}>
+            <Elements>
+              <FavouriteRoundedIcon />
+              <NavText>Favourites</NavText>
+            </Elements>
+          </Link>
+        )}
       </div>
     </div>
   );
